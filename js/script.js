12d8b810efca2307c54d5c0855c32107bd54b80b
sf-002 <sf-002@ya.ru>
2020-07-22 12:27:32 +0300

    first work (lesson 012)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-console.log('test');
+"use strict";
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let oneLastFilm;
+let rateOfOneLastFilm;
+let numberOfRateFilms = 0;
+
+for (let i = 0, numberOfRateFilmsTemp = numberOfRateFilms; 
+    i < numberOfFilms - numberOfRateFilmsTemp; i++) {
+    
+    oneLastFilm = prompt("Один из последних просмотренных фильмов?", "");
+    rateOfOneLastFilm = prompt("На сколько оцените его?", 
+    "\"000\" для выхода", "");
+    if ((oneLastFilm || rateOfOneLastFilm) === "000") {
+        break;
+    }
+    personalMovieDB.movies[oneLastFilm] = rateOfOneLastFilm;
+    numberOfRateFilms++;
+}
+
+console.log(personalMovieDB);
