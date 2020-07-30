22728498ea38ac651717e93a1fe4edf3e744536e
sf-002 <sf-002@ya.ru>
2020-07-30 20:48:49 +0300

    Change eval to callback-function
    Replaced eval to callback-function at promptWhile, added compare-function.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 
 let 
     numberOfFilms,
-    //База данных пользователя
     personalMovieDB = {
     count: Number,
     movies: {},
@@ -10,17 +9,15 @@ let
     genres: [],
     privat: false
     },
-    oneLastFilm, //Имя просмотренного фильма
-    rateOfOneLastFilm, //Оценка данного фильма
-    numberOfRateFilms = 0 //Количество оцененных фильмов
+    oneLastFilm, 
+    rateOfOneLastFilm, 
+    numberOfRateFilms = 0 
     ;
 
 function start() {
-    console.log(`start() is started`);
     do {
         numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
     } while (promptErrNumCheck(numberOfFilms));
-    console.log(`finished start() with numberOfFilms = ${numberOfFilms}`);
 }
 
 const 
@@ -28,96 +25,95 @@ const
     promptErrNum = 0;
 
 function promptErrCheck(textVar) {
-    console.log(`promptErrCheck(${textVar}) is started `);
     return (textVar === null || textVar == "" || /^\s+$/.test(textVar));
 }
 
 function promptErrNumCheck(numberVar) {
-    console.log(`promptErrNumCheck(${numberVar}) is started `);
     return (promptErrCheck(numberVar) || isNaN(numberVar));
 }
 
+//Не используется (была вырезана)
 function lengthF(str) {
     return str.length;
 }
 
-let fParam
-    ;
+function compare(compared, operation, reference){
+    if (isNaN(compared)) {
+        compared = compared.length;
+    }
+    switch (operation) {
+        case '<':
+            return (compared < reference);
+        case '<=':
+            return (compared <= reference);
+        case '==':
+            return (compared == reference);
+        case '===':
+            return (compared === reference);
+        case '>=':
+            return (compared >= reference);
+        case '>':
+            return (compared > reference);
+        default:
+            console.log('function "compare" accepted an unknow operator');
+            return false;
+    }
+}
 
 /**
  * Запрашивает данные вплоть до выполнения задданых условий, данные записываются в глобальную переменную fParam
  *
  * @param {String} text Выводимый в вопросе текст
- * @param {Array<String>} verificationСonditions Перечень запретов в виде функций с параметром fParam
- * @param {Array<String>} alerts Перечень предупреждений
+ * @param {Array<Array<Function, String>>} verificationСonditions Перечень запретов в виде массива с массивами, 
+ * содержащих функцию и значения ее параметров, начиная со второго, первым аргументом передается ответ пользователя.
+ * @param {Array<String>} alerts Перечень выводимых предупреждений
  * @return {String} Ответ пользователя
  */
 function promptWhile(text, verificationСonditions, alerts) {
-    //perr perrnum 
-    console.log(`promptWhile(${text}, ${verificationСonditions}, ${alerts}) is started `);
-    console.log(verificationСonditions);
-    let temp,
-        lenVC = verificationСonditions.length
+    let 
+        lengthVC = verificationСonditions.length,
+        answer
     ;
-    out11: while(true) { 
-        console.log('while is started');
-        fParam = prompt(text, "");
-        for (let i = 0; i < lenVC; i++) {
-            //let fName = verificationСonditions[i];
-            //let VCFunc = eval(verificationСonditions[i]);
-            //console.log(fParam, verificationСonditions[i], VCFunc);
-            console.log(`for is started, ${i} iteration`);
+
+    out1: while(true) { 
+        answer = prompt(text, "");
+        
+        for (let i = 0; i < lengthVC; i++) {
             switch (true) {
-                case eval(verificationСonditions[i]):
+                //verificationСonditions - массив из массивов. Каждый i-й элемент содержит массив-условие, 
+                //нулевым элементом которого является проверяющая фунция, остальными - условия проверки ответа
+                case verificationСonditions[i][0](answer, ...verificationСonditions[i].slice(1)):
                     alert(alerts[i]);
-                    console.log('case swithc, alert is true');
-                    continue out11;
+                    continue out1;
                 default:
-                    console.log('default switch');
                     break; 
             }
-            console.log(`for is finished, ${i} iteration`);
         }
+
         break;
     } 
-    console.log(`finished promptWhile() with return is ${fParam}`);
-    return fParam;
+    return answer;
 }
 
-//console.log( Object.keys({}).length);
+
 function rememberMyFilms(DBName) {
-    console.log(`rememberMyFilms(${DBName}) is started `);
-//Данная функция проводит опрос пользователя на тему просмотренных фильмов вплоть до оценки всех неоценных фильмов 
-//и выполнения предъявляемых требований к названию фильма и его оценке, сохраняя ответы в базе пользователя
     let numberOfRateFilms = Object.keys(DBName.movies).length;
-    console.log(`numberOfRateFilms = ${numberOfRateFilms}`);
     //Количество незаполненных фильмов итераций
     out1: for (let i = 0, temp = numberOfFilms - numberOfRateFilms; i < temp; i++) {
-        console.log(`for(i = 0; ${i} < ${temp}; i++)`);
-        //Ввод названия фильма вплоть до выполнения требований к имени
-        oneLastFilm = promptWhile('Один из последних просмотренных фильмов?', 
-                        ['promptErrCheck(fParam)', 'lengthF(fParam) > 50'], 
-                        ['Необходимо указать все фильмы', 'Не более 50 символов в названии']);
-        console.log(`oneLastFilm = ${oneLastFilm}`);
+        oneLastFilm = promptWhile( 'Один из последних просмотренных фильмов?', 
+                        [ [promptErrCheck], [compare, '>', 50] ], 
+                        ['Необходимо указать все фильмы', 'Не более 50 символов в названии'] );
         const str = 'Укажите оценку от 1 до 10';
-        //Ввод оценки фильма вплоть до выполнения требований к имени
-        rateOfOneLastFilm = promptWhile('На сколько оцените его?', 
-                                ['promptErrNumCheck(fParam)', 'fParam < 1', 'fParam > 10'],
-                                [str, str, str]); 
-        console.log(`rateOfOneLastFilm = ${rateOfOneLastFilm}`);
-        console.log(`oneLastFilm in DBName.movies is ${oneLastFilm} in ${DBName.movies} is',
-                    (oneLastFilm in DBName.movies)`);
-        //Проверка на повтор о оценках фильмов
+        rateOfOneLastFilm = promptWhile( 'На сколько оцените его?', 
+                                [ [promptErrNumCheck], [compare, '<', 1], [compare, '>', 10] ],
+                                [str, str, str] ); 
         if (oneLastFilm in DBName.movies) {
             i--;
             alert(`Фильму "${oneLastFilm}" присвоена новая оценка`);
-            console.log(oneLastFilm);
         }
         DBName.movies[oneLastFilm] = rateOfOneLastFilm;
         DBName.count++;
-        console.log(`${i} iteration is finished`);
     }
-    console.log(`finished rememberMyFilms(); DBname is ${DBName}`);
 }
 
 function detectPersonalLevel(DBname) {
@@ -149,13 +145,6 @@ function writeYourGenres(DBname) {
     }
 }
 
-/*
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - 
-вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический 
-зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-*/
 start();
 if (prompt('rememberMyFilms', '') !== null) {
     rememberMyFilms(personalMovieDB);
