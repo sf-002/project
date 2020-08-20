b053a62c08cffdcb344fc49bc9593a9e2a37a0f8
sf-002 <sf-002@ya.ru>
2020-08-20 17:52:54 +0300

    5th work,  replaced files with new project
    got new html and css files, a few changes on the index page with script

--- js/script.js
+++ js/script.js
@@ -1,163 +1,37 @@
-"use strict";
+/* Задания на урок:
 
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
-let personalMovieDB = {
-        count: 0,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false,
+2) Изменить жанр фильма, поменять "комедия" на "драма"
 
-        start: function() {
-            let numberOfFilms;
-            do {
-                numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-            } while (personalMovieDB.promptErrNumCheck(numberOfFilms));
-            this.count += numberOfFilms;
-        },
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
 
-        rememberMyFilms: function() {
-            console.log('rememberMyFilms', this);
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
 
-            let numberOfRateFilms = Object.keys(this.movies).length,
-                oneLastFilm, 
-                rateOfOneLastFilm
-            ;
-            //Количество незаполненных фильмов итераций
-            out1: for (let i = 0, temp = this.count - numberOfRateFilms; i < temp; i++) {
-                oneLastFilm = personalMovieDB.promptWhile( 'Один из последних просмотренных фильмов?', 
-                                [[personalMovieDB.promptErrCheck], [personalMovieDB.compare, '>', 50]], 
-                                ['Необходимо указать все фильмы', 'Не более 50 символов в названии'] );
-                const str = 'Укажите оценку от 1 до 10';
-                rateOfOneLastFilm = personalMovieDB.promptWhile( 'На сколько оцените его?', 
-                                        [[personalMovieDB.promptErrNumCheck], 
-                                        [personalMovieDB.compare, '<', 1], [personalMovieDB.compare, '>', 10]],
-                                        [str, str, str] ); 
-                if (oneLastFilm in this.movies) {
-                    i--;
-                    alert(`Фильму "${oneLastFilm}" присвоена новая оценка`);
-                }
-                this.movies[oneLastFilm] = rateOfOneLastFilm;
-                //this.count++;
-            }
-        },
-        
-        detectPersonalLevel: function() {
-            switch (true) {
-                default:
-                case isNaN(this.count):
-                    alert("Произошла ошибка");
-                    break;
-                case this.count < 10:
-                    alert("Просмотрено довольно мало фильмов");
-                    break;
-                case this.count <= 30:
-                    alert("Вы классический зритель");
-                    break;
-                case this.count > 30:
-                    alert("Вы киноман");
-            }
-        },
-        
-        showMyDB: function() {
-            if (!this.private) {
-                console.log(this);
-            }
-        },
-        
-        writeYourGenres: function() {
-            let currentGenre = '';
-            for (let i = 0; i < 3; i++) {
-                currentGenre = personalMovieDB.promptWhile(`Ваш любимый жанр под номером ${i+1}`,
-                                     [[personalMovieDB.promptErrCheck]], ['Введите жанр']);
-                if (this.genres.includes(currentGenre, 0)) {
-                    i--;
-                    alert(`Жанр "${currentGenre}" уже был указан ранее`);
-                } else {
-                    this.genres[i] = currentGenre;
-                }
-            }
-            this.genres.forEach(function(item, index, arr) {
-                console.log(`Любимый жанр #${index+1} - это ${item}`);
-            });
-        },
+5) Добавить нумерацию выведенных фильмов */
 
-        toggleVisibleMyDB: function() {
-            this.privat = !this.privat;
-        },
+'use strict';
 
-        //accessory
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
 
-        promptErrCheck: function (textVar) {
-            return (textVar === null || textVar == "" || /^\s+$/.test(textVar));
-        },
-        
-        promptErrNumCheck: function (numberVar) {
-            return (personalMovieDB.promptErrCheck(numberVar) || isNaN(numberVar));
-        },
-        
-        compare: function (compared, operation, reference) {
-            if (isNaN(compared)) {
-                compared = compared.length;
-            }
-            switch (operation) {
-                case '<':
-                    return (compared < reference);
-                case '<=':
-                    return (compared <= reference);
-                case '==':
-                    return (compared == reference);
-                case '===':
-                    return (compared === reference);
-                case '>=':
-                    return (compared >= reference);
-                case '>':
-                    return (compared > reference);
-                default:
-                    console.log('function "compare" accepted an unknow operator');
-                    return false;
-            }
-        },
+console.log(document.querySelector('.promo__bg').style.background);
+document.querySelector('.promo__adv').remove();
+document.querySelector('.promo__content').style.width = "100%";
+document.querySelector('.promo__genre').textContent = "ДРАМА";
+document.querySelector('.promo__bg').style.cssText = 
+    "background: url('../img/bg.jpg') center center/cover no-repeat";  
+movieDB.movies.sort();
+document.querySelectorAll('.promo__interactive-item').forEach ((item, i) => {
+    item.textContent = `${i+1}. ${movieDB.movies[i]}`;
+});
 
-        /**
-         * Запрашивает данные вплоть до выполнения задданых условий, данные записываются в глобальную переменную fParam
-         *
-         * @param {String} text Выводимый в вопросе текст
-         * @param {Array<Array<Function, String>>} verificationСonditions Перечень запретов в виде массива с массивами, 
-         * содержащих функцию и значения ее параметров, начиная со второго, первым аргументом передается ответ пользователя.
-         * @param {Array<String>} alerts Перечень выводимых предупреждений
-         * @return {String} Ответ пользователя
-         */
-        promptWhile: function (text, verificationСonditions, alerts) {
-            let 
-                lengthVC = verificationСonditions.length,
-                answer
-            ;
-        
-            out1: while(true) { 
-                answer = prompt(text, "");
-                for (let i = 0; i < lengthVC; i++) {
-                    switch (true) {
-                        //verificationСonditions - массив из массивов. Каждый i-й элемент содержит массив-условие, 
-                        //нулевым элементом которого является проверяющая фунция, остальными - условия проверки ответа
-                        case verificationСonditions[i][0](answer, ...verificationСonditions[i].slice(1)):
-                            alert(alerts[i]);
-                            continue out1;
-                        default:
-                            break; 
-                    }
-                }
-            
-                break;
-            } 
-            return answer;
-        },
-        
-    }
-;
-
-// personalMovieDB.start();
-// personalMovieDB.rememberMyFilms();
-// personalMovieDB.detectPersonalLevel();
-
-console.log(personalMovieDB);
