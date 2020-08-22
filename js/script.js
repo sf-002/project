7d25981bc6a1b6a5b4253313fa859ae56b65cf23
sf-002 <sf-002@ya.ru>
2020-08-22 21:56:54 +0300

    6th work (lesson 033)
    Added scripts to append a new film and remove them.

--- js/script.js
+++ js/script.js
@@ -30,8 +30,55 @@ document.querySelector('.promo__content').style.width = "100%";
 document.querySelector('.promo__genre').textContent = "ДРАМА";
 document.querySelector('.promo__bg').style.cssText = 
     "background: url('../img/bg.jpg') center center/cover no-repeat";  
-movieDB.movies.sort();
-document.querySelectorAll('.promo__interactive-item').forEach ((item, i) => {
-    item.textContent = `${i+1}. ${movieDB.movies[i]}`;
+
+
+console.log(document.querySelector('.add').querySelector('button'));
+
+const 
+    addForm = document.querySelector('.add'),
+    addBtn = addForm.querySelector('button'),
+    addFavoriteCheck = addForm.querySelector('input[type="checkbox"]'), //checked
+    addNameOfFilm = addForm.querySelector('.adding__input')
+    //deleteBtns = document.querySelectorAll('.delete')
+;
+
+function displayFilms() {
+    movieDB.movies.sort();
+    document.querySelectorAll('.promo__interactive-item').forEach ((item, i) => {
+        item.firstChild.data = `${i+1}. ${movieDB.movies[i]} `;
+    });
+}
+
+function rememberMyFilms(e) {
+    e.preventDefault();
+    let nameOfFilm = addNameOfFilm.value;
+    if (nameOfFilm.length > 21) {
+        movieDB.movies.push(nameOfFilm.slice(0, 22-3) + '...');
+        
+    } else {
+        movieDB.movies.push(nameOfFilm);
+    }
+    movieDB.movies.sort();
+    if (addFavoriteCheck.checked) {
+        console.log('Добавляем любимый фильм');
+    }
+    displayFilms();
+}
+
+function deleteFilm(e) {
+    //e.target.parentElement.textContent.slice(0, 1)
+    console.log('click');
+    console.log(e.target.parentElement.textContent.slice(0, 1) - 1);
+    console.log(movieDB.movies);
+    movieDB.movies.splice(e.target.parentElement.textContent.slice(0, 1) - 1, 1);
+    
+    //console.log(movieDB.movies.slice(e.target.parentElement.textContent.slice(0, 1) - 1, 1));
+    displayFilms();
+}
+
+displayFilms();
+addBtn.addEventListener('click', rememberMyFilms);
+document.querySelectorAll('.delete').forEach((item, i) => {
+    item.addEventListener('click', deleteFilm);
 });
 
